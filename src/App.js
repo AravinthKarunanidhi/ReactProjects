c17530ec2dc99ecddb01df820a6e2194274798a9
AravinthKarunanidhi <aravinthinangu@gmail.com>
2022-04-19 21:14:10 +0530

    first app

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Todo from "./components/Todo";
+
+function App() {
+  return (
+    <div>
+      <h1>My Todos</h1>
+      <Todo text="learn react" />
+      <Todo text="learn stack" />
+      <Todo text="learn mern" />
+  
+    </div>
+  );
+}
+
+export default App;
